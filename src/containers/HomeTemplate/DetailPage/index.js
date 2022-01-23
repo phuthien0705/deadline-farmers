a2694ce30127507eea5a8c6be876b631a54cb0b3
SangCuTo <sangn6767@gmail.com>
2022-01-23 15:43:27 +0700

    ReBuil_DetailPages

--- src/containers/HomeTemplate/DetailPage/index.js
+++ src/containers/HomeTemplate/DetailPage/index.js
@@ -1,60 +1,43 @@
-import React, {useContext, useState, useEffect} from 'react'
-import {useParams, Link} from 'react-router-dom'
-import {GlobalState} from '../../../GlobalState'
-import ProductItem from './productItem/ProductItem'
-
+import React, { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import axios from 'axios'
 
 function DetailProduct() {
-    const params = useParams()
-    const state = useContext(GlobalState)
-    const [products] = state.productsAPI.products
-    const addCart = state.userAPI.addCart
     const [detailProduct, setDetailProduct] = useState([])
-
-    useEffect(() =>{
-        if(params.id){
-
-            products.forEach(product => {
-                if(product._id === params.id) setDetailProduct(product)
-            })
+    const setProduct = async ( )=> 
+    {
+        const res = await axios.get(`http://68.183.224.29:5000/api/v1/product/:id [GET]`)
+        return res.data;
+    }
+   useEffect(()=>{
+        const getProducts = async () => {
+            const allProducts = await setProduct()
+            if (allProducts) setDetailProduct(detailProduct)
         }
-    },[params.id, products])
+        getProducts()
+   },[])
 
-    if(detailProduct.length === 0) return null;
-
-    return (
-        <div>
-            <div className="detail">
-                <img src={detailProduct.images.url} alt="" />
-                <div className="box-detail">
-                    <div className="row">
-                        <h2>{detailProduct.title}</h2>
-                        <h6>#id: {detailProduct.product_id}</h6>
-                    </div>
-                    <span>$ {detailProduct.price}</span>
-                    <p>{detailProduct.description}</p>
-                    <p>{detailProduct.content}</p>
-                    <p>Sold: {detailProduct.sold}</p>
-                    <Link to="/cart" className="cart"
-                    onClick={() => addCart(detailProduct)}>
-                        Buy Now
-                    </Link>
-                </div>
-            </div>
-
+    
+        return (
             <div>
-                <h2>Related products</h2>
-                <div className="products">
-                    {
-                        products.map(product => {
-                            return product.category === detailProduct.category 
-                                ? <ProductItem key={product._id} product={product} /> : null
-                        })
-                    }
+                <div className="detail">
+                    <img src={detailProduct.images.url} alt="" />
+                    <div className="box-detail">
+                        <div className="row">
+                            <h2>{detailProduct.title}</h2>
+                            <h6>#id: {detailProduct.product_id}</h6>
+                        </div>
+                        <span>$ {detailProduct.price}</span>
+                        <p>{detailProduct.description}</p>
+                        <p>{detailProduct.content}</p>
+                        <p>Sold: {detailProduct.sold}</p>
+                        
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
 }
+
+
 
 export default DetailProduct
--- src/containers/HomeTemplate/HomePage/components/PostList/index.jsx
+++ src/containers/HomeTemplate/HomePage/components/PostList/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/Post.css';
-
+import {Link} from 'react-router-dom'
 PostList.propTypes = {
     posts: PropTypes.array,
 };
@@ -12,14 +12,17 @@ PostList.defaultProps = {
 
 function PostList(props) {
     const {posts} = props;
-    
+    console.log(posts)
     return (
         <div>
             <ul className="post-list grid grid-cols-2 mt-5">
                 {posts.map(post => (
                     <li key={post.id} className="post">
                         <img src={post.imageUrl} alt="" />
+                        <Link to={`/detail/${post.id}`}>
+
                         <h2 className='post-title'>{post.title}</h2>
+                        </Link>
                         <h4 className='post-author'>{post.author}</h4>
                         <button className='post-add'>Add to cart</button>
                     </li>
